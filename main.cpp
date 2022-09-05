04279994a42f56e909abc14a2819cbb90079cbac
SophieA3 <112896540+SophieA3@users.noreply.github.com>
2022-09-05 11:49:36 -0600

    Formatted Document

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+#include <stdio.h>
+
+int main()
+{
+    int x = 4;
+    float y = 4.4;
+    printf("%d", x);
+    printf("%f", y)   //
+   printf("XYZ") // string
+
+        cout
+        << x << " " << y << " "
+        << "ABC" << endl;
+    return 0;
+}
