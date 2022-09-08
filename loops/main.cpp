0b2f539df27f104076bed7b7518d6757cdf6f9fe
SophieA3 <sophiea3@users.noreply.github.com>
2022-09-08 16:37:59 -0600

    Visual Studio Code Config

--- loops/main.cpp
+++ loops/main.cpp
@@ -7,11 +7,10 @@ int main()
     int x = 4;
     float y = 4.4;
     printf("%d", x);
-    printf("%f", y)   //
-   printf("XYZ") // string
+    printf("%f", y); //
+    printf("XYZ");   // string
 
-        cout
-        << x << " " << y << " "
-        << "ABC" << endl;
+    cout << x << " " << y << " "
+         << "ABC" << endl;
     return 0;
 }
